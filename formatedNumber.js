9e7ef9d2b0b085022b9bb19519eac3bb398e1ae0
vitor <vitorcastanhos@gmail.com>
2023-12-31 20:24:36 -0300

    trying things on JS

---
+++ formatedNumber.js
@@ -0,0 +1,15 @@
+// const inputField = document.querySelector("input");
+
+// inputField.onkeyup = function () {
+//   const removeCharacters = this.value.replace(/[^0-9.]/g, ""); // This removes alphabets and special characters
+
+//   const removeDots = removeCharacters.replace(/\./g, ""); // This is to remove "DOT"
+
+//   const formatedNumber = removeDots.replace(/\B(?=(\d{2})+(?!\d))/g, ","); // This is to
+//   this.value = formatedNumber;
+
+//   const formNumber = Number(formatedNumber);
+//   formNumber;
+// };
+
+// export default inputField;
